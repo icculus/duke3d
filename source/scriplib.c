c8f84abecec8cb54fac630bd0f5f8008c937ec1f
theoddone33 <theoddone33@06e23c4e-721e-0410-8d7f-aef44a64324d>
2003-04-02 05:47:56 +0000

    Implemented most of scriplib.c
    Things left to fix:
    - Possible bug in freeing
    - Choose first available handle instead of just incrementing
    - Test test test, not very robust error checking

--- source/scriplib.c
+++ source/scriplib.c
@@ -24,12 +24,256 @@ Prepared for public release: 03/21/2003 - Charlie Wiederhold, 3D Realms
 */
 //-------------------------------------------------------------------------
 
+#include <assert.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "duke3d.h"
 #include "scriplib.h"
+
+/* #define DEBUG_SCRIPLIB */
+
+#define MAX_SCRIPTS 5
+
+typedef enum {
+	SCRIPTFLAG_UNKNOWN,
+	SCRIPTFLAG_CATEGORY,
+	SCRIPTFLAG_ONESTRING,
+	SCRIPTFLAG_TWOSTRING,
+	SCRIPTFLAG_NUMBER
+} scriptflag_t;
+
+typedef struct scriptnode_s {
+	struct scriptnode_s *child;
+	struct scriptnode_s *sibling;
+	char *key;
+	scriptflag_t type;
+	union {
+		char *string[2];
+		int number;
+	} data;
+} scriptnode_t;
+
+static int script_nexthandle = 0;
+static int script_numscriptsopen = 0;
+
+static scriptnode_t *script_headnode[MAX_SCRIPTS];
+
+/* Utility functions */
+static scriptnode_t *SCRIPT_constructnode (void)
+{
+	scriptnode_t *s;
+
+	s = (scriptnode_t *) malloc (sizeof (scriptnode_t));
+	if (s != NULL)
+	{
+		s->child = NULL;
+		s->sibling = NULL;
+		s->key = NULL;
+		s->data.string[0] = NULL;
+		s->data.string[1] = NULL;
+		s->type = SCRIPTFLAG_UNKNOWN;
+	}
+	return s;
+}
+
+static void SCRIPT_freenode (scriptnode_t *node)
+{
+	assert (node != NULL);
+
+	if (node->type == SCRIPTFLAG_ONESTRING) {
+		free (node->data.string[0]);
+	} else if (node->type == SCRIPTFLAG_TWOSTRING) {
+		free (node->data.string[0]);
+		free (node->data.string[1]);
+	}
+
+	free (node->key);
+	free (node->sibling);
+	free (node->child);
+	free (node);
+	node = NULL;
+}
+
+static void SCRIPT_recursivefree (scriptnode_t *node)
+{
+	assert (node != NULL);
+
+	/* Free dependant nodes first */
+	if (node->child) {
+		SCRIPT_recursivefree (node->child);
+	}
+
+	if (node->sibling) {
+		SCRIPT_recursivefree (node->sibling);
+	}
+
+	SCRIPT_freenode (node);
+}
+
+static void SCRIPT_addsibling (scriptnode_t *node, scriptnode_t *sibling)
+{
+	assert (node != NULL);
+	assert (sibling != NULL);
+	
+	/* printf ("addsib: %p, %p, %p\n", node, node->sibling, sibling); */
+
+	if (node->sibling == NULL) {
+		node->sibling = sibling;
+	} else {
+		SCRIPT_addsibling (node->sibling, sibling);
+	}
+}
+
+static void SCRIPT_addchild (scriptnode_t *parent, scriptnode_t *child)
+{
+	assert (parent != NULL);
+	assert (child != NULL);
+
+	if (parent->child == NULL) {
+		parent->child = child;
+	} else {
+		SCRIPT_addsibling (parent->child, child);
+	}
+}
+
+static char *SCRIPT_copystring (char * s)
+{
+	char *ret;
+
+	ret = (char *) malloc (strlen (s)+1);
+	if (ret != NULL)
+	{
+		strcpy (ret, s);
+	}
+	return ret;
+}
+
+static int SCRIPT_getnexttoken (char *buffer, char* token, int start)
+{
+	int iterator = start;
+
+	if (buffer[start] == '\0') {
+		token[0] = '\0';
+		return start;
+	}
+
+	while (iterator < 128 && !isspace (buffer[iterator]))
+	{
+		token[iterator-start] = buffer[iterator];
+		iterator++;
+	}
+
+	token[iterator-start] = '\0';
+
+	/* Trim off any extra whitespace */
+	while (iterator < 127 && isspace(buffer[iterator+1]))
+		iterator++;
+
+	return ++iterator;
+}
+
+/* Fills in a scriptnode with the interpreted contents of a line */
+static void SCRIPT_parseline (char *curline, scriptnode_t *node)
+{
+	char token[128];
+	int i;
+
+	/* Needs to handle 5 cases: */
+	/* 	key = someint */
+	/* 	key = 0xsomehexnum */
+	/* 	key = ~ */
+	/* 	key = "onestring" */
+	/* 	key = "two" "strings" */
+
+	assert (node != NULL);
+	assert (curline != NULL);
+
+	i = SCRIPT_getnexttoken (curline, token, 0);
+	node->key = SCRIPT_copystring (token);
+
+	i = SCRIPT_getnexttoken (curline, token, i);
+	/* Sanity check... this token should be "=" */
+	if (strcmp (token, "=")) {
+		/* Error state, free the memory allocated */
+		SCRIPT_recursivefree (node);
+		return;
+	}
+
+	/* This is where the real fun begins... */
+	/* we can begin to determine which of the 5 */
+	/* possibilities the node is now */
+	i = SCRIPT_getnexttoken (curline, token, i);
+
+	if (!strncmp (token, "0x", 2)) {
+		/* Found a hex digit! */
+		node->type = SCRIPTFLAG_NUMBER;
+		node->data.number = strtol (token, NULL, 16);
+	} else if (isdigit (token[0])) {
+		/* Found a number! */
+		node->type = SCRIPTFLAG_NUMBER;
+		node->data.number = atoi (token);
+	} else if (token[0] == '~') {
+		/* Found a ... who knows */
+		node->type = SCRIPTFLAG_NUMBER;
+		node->data.number = -1;
+	} else if (token[0] == '"') {
+		char workbuf[128];
+		int r;
+
+		/* Found one of possibly two strings */
+		strcpy (workbuf, token);
+		while (token != NULL && workbuf[strlen(workbuf)-2] != '"')
+		{
+			i = SCRIPT_getnexttoken (curline, token, i);
+			strcat (workbuf, " ");
+			strcat (workbuf, token);
+		}
+		r = sscanf(workbuf, "\"%[^\"]\"", workbuf);
+		if (r == 0) workbuf[0] = '\0';
+
+		node->type = SCRIPTFLAG_ONESTRING;
+		node->data.string[0] = SCRIPT_copystring (workbuf);
+		/* Check for a second string */
+		i = SCRIPT_getnexttoken (curline, token, i);
+		if (token[0] == '"') {
+			strcpy (workbuf, token);
+			while (token != NULL && workbuf[strlen(workbuf)-2] != '"')
+			{
+				i = SCRIPT_getnexttoken (curline, token, i);
+				strcat (workbuf, " ");
+				strcat (workbuf, token);
+			}
+			r = sscanf(workbuf, "\"%[^\"]\"", workbuf);
+			if (r == 0) workbuf[0] = '\0';
+
+			node->type = SCRIPTFLAG_TWOSTRING;
+			node->data.string[1] = SCRIPT_copystring (workbuf);
+		}
+	} else {
+		/* Error state! */
+		SCRIPT_recursivefree (node);
+	}
+}
+
+static scriptnode_t *SCRIPT_findinchildren (scriptnode_t *parent, char *s)
+{
+	scriptnode_t *cur = parent;
+
+	if (cur == NULL) return NULL;
+	cur = cur->child;
+	if (cur == NULL) return NULL;
+	while (cur != NULL)
+	{
+		if (!strcmp(cur->key, s))
+			break;
+		cur = cur->sibling;
+	}
+
+	return cur;
+}
 
 /*
 ==============
@@ -55,7 +299,11 @@ int32 SCRIPT_Init( char * name )
 */
 void SCRIPT_Free( int32 scripthandle )
 {
-	STUBBED("Free");
+	/* STUBBED("Free"); */
+	if (scripthandle == -1) return;
+
+	SCRIPT_recursivefree (script_headnode[scripthandle]);
+	script_numscriptsopen--;
 }
 
 
@@ -85,9 +333,86 @@ int32 SCRIPT_Parse ( char *data, int32 length, char * name )
 
 int32 SCRIPT_Load ( char * filename )
 {
-	STUBBED("Load");
-	
-	return -1;
+	FILE *fp;
+	char curline[128];
+	scriptnode_t *headnode = NULL;
+	scriptnode_t *cur_subsection = NULL;
+
+	if (script_numscriptsopen == MAX_SCRIPTS) return -1;
+
+	/* The main program does not check for any sort of */
+	/* error in loading, so each SCRIPT_ function needs */
+	/* to check if the handle is -1 before doing anything */
+	fp = fopen (filename, "r");
+
+	if (fp == NULL) return -1;
+
+	/* Start loading the script */
+	/* Loads and parse the entire file into a tree */
+	script_numscriptsopen++;
+
+	/* script_nexthandle is the current handle until we increment it */
+	script_headnode[script_nexthandle] = SCRIPT_constructnode ();
+	headnode = script_headnode[script_nexthandle];
+
+	while (!feof (fp))
+	{
+		fgets (curline, 128, fp);
+		
+		/* Skip comments */
+		if (curline[0] == ';') continue;
+
+		/* Parse line */
+		/* We have two options... it starts with a [, making it */
+		/* a new subsection (child of headnode) or it starts with */
+		/* a letter, making it a child of a subsection. */
+		if (curline[0] == '[')
+		{
+			scriptnode_t *node;
+			int i;
+			
+			/* Remove [] manually */
+			for (i = 0; i < 127; i++)
+				curline[i] = curline[i+1];
+			for (i = 127; i >= 0; i--)
+			{
+				if (curline[i] == ']') {
+					curline[i] = '\0';
+					break;
+				} else {
+					curline[i] = '\0';
+				}
+			}
+
+			/* Insert into head */
+			node = SCRIPT_constructnode ();
+			node->type = SCRIPTFLAG_CATEGORY;
+			node->key = SCRIPT_copystring (curline);
+			SCRIPT_addchild (headnode, node);
+			cur_subsection = node;
+			/* printf ("Working in section \"%s\"\n", node->key); */
+		} else if (isalpha (curline[0])) {
+			scriptnode_t *node;
+
+			/* Ignore if not under a subsection */
+			if (cur_subsection == NULL)
+				continue;
+
+			node = SCRIPT_constructnode ();
+			
+			/* TODO: Parse line here */
+			SCRIPT_parseline (curline, node);
+			if (node != NULL)
+			{
+				/* printf ("Adding node with key \"%s\"\n", node->key); */
+				SCRIPT_addchild (cur_subsection, node);
+			}
+		}
+	}
+
+	fclose (fp);
+
+	return script_nexthandle++;	/* postincrement is important here */
 }
 
 /*
@@ -194,7 +519,21 @@ void SCRIPT_GetString
    char * dest
    )
 {
-	STUBBED("GetString");
+	/* STUBBED("GetString"); */
+	if (scripthandle == -1) return;
+
+	scriptnode_t *cur = script_headnode[scripthandle];
+
+	cur = SCRIPT_findinchildren (cur, sectionname);
+	cur = SCRIPT_findinchildren (cur, entryname);
+
+	if (cur != NULL && cur->type == SCRIPTFLAG_ONESTRING)
+	{
+		strcpy (dest, cur->data.string[0]);
+#ifdef DEBUG_SCRIPLIB
+		printf ("GetString: value for %s:%s is %s\n", sectionname, entryname, dest);
+#endif
+	}
 }
 
 /*
@@ -213,7 +552,22 @@ void SCRIPT_GetDoubleString
    char * dest2
    )
 {
-	STUBBED("GetDoubleString");
+	/* STUBBED("GetDoubleString"); */
+	if (scripthandle == -1) return;
+
+	scriptnode_t *cur = script_headnode[scripthandle];
+
+	cur = SCRIPT_findinchildren (cur, sectionname);
+	cur = SCRIPT_findinchildren (cur, entryname);
+
+	if (cur != NULL && cur->type == SCRIPTFLAG_TWOSTRING)
+	{
+		strcpy (dest1, cur->data.string[0]);
+		strcpy (dest2, cur->data.string[1]);
+#ifdef DEBUG_SCRIPLIB
+		printf ("GetDoubleString: value for %s:%s is %s %s\n", sectionname, entryname, dest1, dest2);
+#endif
+	}
 }
 
 /*
@@ -231,9 +585,23 @@ boolean SCRIPT_GetNumber
    int32 * number
    )
 {
-	STUBBED("GetNumber");
-	
-	return false;
+	/* STUBBED("GetNumber"); */
+	if (scripthandle == -1) return false;
+
+	scriptnode_t *cur = script_headnode[scripthandle];
+
+	cur = SCRIPT_findinchildren (cur, sectionname);
+	cur = SCRIPT_findinchildren (cur, entryname);
+
+	if (cur != NULL && cur->type == SCRIPTFLAG_NUMBER)
+	{
+		*number = cur->data.number;
+#ifdef DEBUG_SCRIPLIB
+		printf ("GetNumber: value for %s:%s is %ld\n", sectionname, entryname, *number);
+#endif
+	}
+
+	return (cur != NULL) ? true : false;
 }
 
 /*
